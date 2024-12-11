2ae563dfc1f158600307261ff5c60d939419f17e
Guilherme-Kaua <guilherme.kaua.matos@gmail.com>
2024-12-10 21:48:55 -0300

    Eeee

---
+++ soma.py
@@ -0,0 +1,8 @@
+a = int(input("Digite seu primeiro número = "))
+b = int(input("Digite seu segundo número = "))
+
+if a % 1 == 0 and b % 1 == 0:
+    x = a + b
+    print(f"X = {x}")
+else:
+    print
